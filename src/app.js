2969596b2716c9b3b472e1fa3259c0feff43d571
Byron Rolle JR <dareal4our@gmail.com>
2025-02-25 00:20:36 +0000

    1.1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function () {
+
+  let pronoun = ["The", "Our", "His", "Her", "My"]
+  let adj = ["great", "big", "amazing", "best", "nicest"]
+  let noun = ["dolpin", "wilds", "lewbowski", "fox"]
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+
+        console.log(pronoun[i] + adj[j] + noun[k] + '.com')
+      }
+    }
+  }
+}
